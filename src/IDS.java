aa98c9b6ad27ac11f1c88e0dc04fb2534f3e563c
DELL <DELL@NOAM>
2017-12-03 12:09:28 +0200

    push

--- src/IDS.java
+++ src/IDS.java
@@ -1,16 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class IDS<E> implements Searcher<E> {
+    private Queue<State<E>> queue;
 
     @Override
     public State<E> search(Searchable<E> searchable) {
+        this.queue = new LinkedList<State<E>>();
         State<E> initial = searchable.getInitialState();
         int depth = 0;
         State<E> found = null;
         while (true) {
+            this.queue.add(initial);
             found = DLS(initial, depth, searchable);
             if (found != null) {
                 return found;
             }
+            this.queue.clear();
             depth++;
         }
     }
@@ -21,22 +27,21 @@ public class IDS<E> implements Searcher<E> {
         }
         if (depth > 0) {
             for (State<E> successor : searchable.getSuccessors(s)) {
-                State<E> found = DLS(successor, depth - 1, searchable);
-                if (found != null) {
-                    State<E> cameFrom = found;
-                    while (cameFrom.getCameFrom() != null) {
-                        cameFrom = cameFrom.getCameFrom();
+                if (!this.queue.contains(successor)) {
+                    State<E> found = DLS(successor, depth - 1, searchable);
+                    if (found != null) {
+                        State<E> cameFrom = found;
+                        while (cameFrom.getCameFrom() != null) {
+                            cameFrom = cameFrom.getCameFrom();
+                        }
+                        cameFrom.setCameFrom(s);
+                        return found;
                     }
-                    cameFrom.setCameFrom(s);
-                    return found;
                 }
             }
+            this.queue.remove(s);
         }
+        this.queue.remove(s);
         return null;
     }
-
-//    private State<E, C> DLSI(State<E, C> s, int depth, Searchable<E, C> searchable) {
-//
-//
-//    }
 }
